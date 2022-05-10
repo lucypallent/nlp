93de2891897db934f31dc274742bc7f18dde1b77
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-05-10 18:27:15 +0100

    update create roberta to put in functions

--- create_roberta.py
+++ create_roberta.py
@@ -9,18 +9,25 @@ from scipy import spatial
 from scipy import sparse
 from sklearn.metrics.pariwise import cosine_similarity
 
-val = pd.read_csv('nlp_csv2/val.csv')
-unique = pd.read_csv('nlp_csv2/unique.csv')
+def create_cos_sim_column(df_pth, sv_pth):
+    # val = pd.read_csv('nlp_csv2/val.csv')
+    val = pd.read_csv(df_pth)
+    unique = pd.read_csv('nlp_csv2/unique.csv')
 
-# merge on articleBody
-val = val.merge(unique[['articleBody', 'rob_articleBody']], how='inner', on='articleBody')
+    # merge on articleBody
+    val = val.merge(unique[['articleBody', 'rob_articleBody']], how='inner', on='articleBody')
 
-val = val.merge(unique[['headline', 'rob_headline']], how='inner', on='headline')
+    val = val.merge(unique[['headline', 'rob_headline']], how='inner', on='headline')
 
-print('successfully merged')
+    print('successfully merged')
 
-val['rob_cos'] = val.apply(lambda row: cosine_similarity(row['rob_articleBody'], row['rob_headline']), axis = 1)
+    val['rob_cos'] = val.apply(lambda row: cosine_similarity(row['rob_articleBody'], row['rob_headline']), axis = 1)
 
-val.to_csv('nlp_csv2/rob_val.csv', index=False)
+    # val.to_csv('nlp_csv2/rob_val.csv', index=False)
+    val.to_csv(sv_pth, index=False)
+
+create_cos_sim_column('nlp_csv2/val.csv', 'nlp_csv2/rob_val.csv')
+# create_cos_sim_column('nlp_csv2/train.csv', 'nlp_csv2/rob_train.csv')
+# create_cos_sim_column('nlp_csv2/test.csv', 'nlp_csv2/rob_test.csv')
 
 print('works')
