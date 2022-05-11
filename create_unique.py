a13f7d3ec53a1711a14220463487c4383f7c7f19
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-05-11 01:01:36 +0100

    remove ref to test

--- create_unique.py
+++ create_unique.py
@@ -8,15 +8,15 @@ import scipy
 from scipy import spatial
 
 train = pd.read_csv('nlp_csv2/tfidf_train.csv')
-test = pd.read_csv('nlp_csv2/tfidf_test.csv')
+# test = pd.read_csv('nlp_csv2/tfidf_test.csv')
 val = pd.read_csv('nlp_csv2/tfidf_val.csv')
 
-train_body = pd.read_csv('nlp_csv2/tfidf_train_body.csv')
+# train_body = pd.read_csv('nlp_csv2/tfidf_train_body.csv')
 
 tra_articleBody = train['articleBody'].tolist()
 tra_headline = train['Headline'].tolist()
-tes_articleBody = test['articleBody'].tolist()
-tes_headline = test['Headline'].tolist()
+# tes_articleBody = test['articleBody'].tolist()
+# tes_headline = test['Headline'].tolist()
 val_articleBody = val['articleBody'].tolist()
 val_headline = val['Headline'].tolist()
 
@@ -31,13 +31,13 @@ val_headline_unique = create_unique_list(val_headline)
 val_articleBody_unique = create_unique_list(val_articleBody)
 tra_headline_unique = create_unique_list(tra_headline)
 tra_articleBody_unique = create_unique_list(tra_articleBody)
-tes_headline_unique = create_unique_list(tes_headline)
-tes_articleBody_unique = create_unique_list(tes_articleBody)
+# tes_headline_unique = create_unique_list(tes_headline)
+# tes_articleBody_unique = create_unique_list(tes_articleBody)
 
 print('create unique')
-headline_unique = val_headline_unique + tra_headline_unique + tes_headline_unique
+headline_unique = val_headline_unique + tra_headline_unique #+ tes_headline_unique
 
-articleBody_unique = val_articleBody_unique + tra_articleBody_unique + tes_articleBody_unique
+articleBody_unique = val_articleBody_unique + tra_articleBody_unique #+ tes_articleBody_unique
 print('create large unique')
 
 df = pd.DataFrame(list(zip(headline_unique, articleBody_unique)), columns =['headline', 'articleBody'])
