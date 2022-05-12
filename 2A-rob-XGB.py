f1ef445adf8d690ee8db8f5fb849f8f186ee3727
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-05-12 01:13:30 +0100

    update with how to load in rob correctly

--- 2A-rob-XGB.py
+++ 2A-rob-XGB.py
@@ -1,7 +1,3 @@
-#### UNFINISHED NO CHANGES HAVE YET BEEN MADE
-# waiting to see if tfidf XGB runs
-
-
 import xgboost as xgb
 import pandas as pd
 import numpy as np
@@ -9,14 +5,36 @@ import numpy as np
 from sklearn.metrics import precision_score, recall_score, accuracy_score
 from sklearn.model_selection import GridSearchCV
 import os
+from ast import literal_eval
+
 
 SAVE_DIR = 'checkpoints/2A-ML'
 
-train_df = pd.read_csv('nlp_csv2/tfidf_train.csv')
-valid_df = pd.read_csv('nlp_csv2/tfidf_val.csv')
+train_df = pd.read_csv('nlp_csv2/rob_train.csv')
+valid_df = pd.read_csv('nlp_csv2/rob_val.csv')
 
-train_df.drop(['Body ID', 'articleBody', 'Headline'], axis=1, inplace=True)
-valid_df.drop(['Body ID', 'articleBody', 'Headline'], axis=1, inplace=True)
+train_df.drop(['Body ID', 'articleBody', 'headline'], axis=1, inplace=True)
+valid_df.drop(['Body ID', 'articleBody', 'headline'], axis=1, inplace=True)
+
+train_df.rob_articleBody = train_df.rob_articleBody.apply(literal_eval)
+train_df.rob_headline = train_df.rob_headline.apply(literal_eval)
+
+valid_df.rob_articleBody = valid_df.rob_articleBody.apply(literal_eval)
+valid_df.rob_headline = valid_df.rob_headline.apply(literal_eval)
+
+train_art = train_df.rob_articleBody.apply(pd.Series)
+train_head = train_df.rob_headline.apply(pd.Series)
+
+val_art = valid_df.rob_articleBody.apply(pd.Series)
+val_head = valid_df.rob_headline.apply(pd.Series)
+
+train_df = train_df.join(train_art)
+train_df = train_df.join(train_head)
+train_df.drop(['rob_articleBody', 'rob_headline'], axis=1, inplace=True)
+
+valid_df = valid_df.join(val_art)
+valid_df = valid_df.join(val_head)
+valid_df.drop(['rob_articleBody', 'rob_headline'], axis=1, inplace=True)
 
 di = {'unrelated': 0, 'discuss': 1, 'agree': 1, 'disagree': 1}
 train_df.replace({'Stance': di}, inplace=True)
@@ -101,6 +119,6 @@ print("Accuracy = {}".format(accuracy_score(Y_valid, best_preds2)))
 
 # save the model_output
 print('saving model')
-model.save_model(os.path.join(SAVE_DIR, 'tfidf-XGB.json'))
-model2.save_model(os.path.join(SAVE_DIR, 'tfidf-XGB2.json'))
+model.save_model(os.path.join(SAVE_DIR, 'rob-XGB.json'))
+model2.save_model(os.path.join(SAVE_DIR, 'rob-XGB2.json'))
 print('model saved')
