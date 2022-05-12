fa70809117557b3b91779ca1fecadf2b9b9ce679
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-05-12 02:19:49 +0100

    remove the grid search for time

--- 2A-rob-XGB.py
+++ 2A-rob-XGB.py
@@ -85,40 +85,40 @@ print("Precision = {}".format(precision_score(Y_valid, best_preds, average='macr
 print("Recall = {}".format(recall_score(Y_valid, best_preds, average='macro')))
 print("Accuracy = {}".format(accuracy_score(Y_valid, best_preds)))
 
-clf = xgb.XGBClassifier()
-parameters = {
-     "eta"    : [0.05, 0.10, 0.15, 0.20, 0.25, 0.30 ] ,
-     "max_depth"        : [ 3, 4, 5, 6, 8, 10, 12, 15],
-     "min_child_weight" : [ 1, 3, 5, 7 ],
-     "gamma"            : [ 0.0, 0.1, 0.2 , 0.3, 0.4 ],
-     "colsample_bytree" : [ 0.3, 0.4, 0.5 , 0.7 ],
-     "num_class"        : [3],
-     'objective'        : ['multi:softprob']
-     }
-
-grid = GridSearchCV(clf,
-                    parameters, n_jobs=4,
-                    scoring="neg_log_loss",
-                    cv=3)
-
-grid.fit(X_train, Y_train)
-
-print("\n The best estimator across ALL searched params:\n",grid.best_estimator_)
-print("\n The best score across ALL searched params:\n",grid.best_score_)
-print("\n The best parameters across ALL searched params:\n",grid.best_params_)
-
-model2 = xgb.train(grid.best_params_, D_train, steps)
-
-preds2 = model2.predict(D_test)
-best_preds2 = np.asarray([np.argmax(line) for line in preds2])
-
-print('best preds')
-print("Precision = {}".format(precision_score(Y_valid, best_preds2, average='macro')))
-print("Recall = {}".format(recall_score(Y_valid, best_preds2, average='macro')))
-print("Accuracy = {}".format(accuracy_score(Y_valid, best_preds2)))
-
+# clf = xgb.XGBClassifier()
+# parameters = {
+#      "eta"    : [0.05, 0.10, 0.15, 0.20, 0.25, 0.30 ] ,
+#      "max_depth"        : [ 3, 4, 5, 6, 8, 10, 12, 15],
+#      "min_child_weight" : [ 1, 3, 5, 7 ],
+#      "gamma"            : [ 0.0, 0.1, 0.2 , 0.3, 0.4 ],
+#      "colsample_bytree" : [ 0.3, 0.4, 0.5 , 0.7 ],
+#      "num_class"        : [3],
+#      'objective'        : ['multi:softprob']
+#      }
+#
+# grid = GridSearchCV(clf,
+#                     parameters, n_jobs=4,
+#                     scoring="neg_log_loss",
+#                     cv=3)
+#
+# grid.fit(X_train, Y_train)
+#
+# print("\n The best estimator across ALL searched params:\n",grid.best_estimator_)
+# print("\n The best score across ALL searched params:\n",grid.best_score_)
+# print("\n The best parameters across ALL searched params:\n",grid.best_params_)
+#
+# model2 = xgb.train(grid.best_params_, D_train, steps)
+#
+# preds2 = model2.predict(D_test)
+# best_preds2 = np.asarray([np.argmax(line) for line in preds2])
+#
+# print('best preds')
+# print("Precision = {}".format(precision_score(Y_valid, best_preds2, average='macro')))
+# print("Recall = {}".format(recall_score(Y_valid, best_preds2, average='macro')))
+# print("Accuracy = {}".format(accuracy_score(Y_valid, best_preds2)))
+#
 # save the model_output
 print('saving model')
 model.save_model(os.path.join(SAVE_DIR, 'rob-XGB.json'))
-model2.save_model(os.path.join(SAVE_DIR, 'rob-XGB2.json'))
+# model2.save_model(os.path.join(SAVE_DIR, 'rob-XGB2.json'))
 print('model saved')
